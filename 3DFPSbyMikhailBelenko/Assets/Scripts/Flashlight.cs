3d1a9cade51be23ce3d758eed66e7b90a8e6d47c
PhoenixKnight <mefisto40000x@gmail.com>
2019-10-19 13:58:35 +0300

    Написан скрипт фонарика

---
+++ 3DFPSbyMikhailBelenko/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : BaseObject
+{
+    private KeyCode control = KeyCode.F;
+
+    private float timeout = 10;
+    private Light _light;
+    private float currentTime;
+    private float currReloadTime;
+    private Material _lightMaterial;
+    
+    void Start()
+    {
+        _light = GetComponentInChildren<Light>();
+        _lightMaterial = GetMaterial;
+    }
+
+    private void ActiveFlashLight(bool val)
+    {
+        _light.enabled = val;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(control))
+        {
+            ActiveFlashLight(_light.enabled ? false : true);
+        }
+
+        if(_light.enabled)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime > timeout)
+            {
+                currentTime = 0;
+                ActiveFlashLight(false);
+            }
+        }
+    }
+}
