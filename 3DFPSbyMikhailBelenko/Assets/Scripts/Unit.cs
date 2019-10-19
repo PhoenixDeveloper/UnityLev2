337047f7c0372442ac654f8de9289dc4e8ff3bf2
PhoenixKnight <mefisto40000x@gmail.com>
2019-10-19 13:42:38 +0300

    Создан класс базового юнита

---
+++ 3DFPSbyMikhailBelenko/Assets/Scripts/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : BaseObject
+{
+    [SerializeField] private float _health;
+
+    public float Health
+    {
+        get
+        {
+            return _health;
+        }
+
+        set
+        {
+            if(value > 0)
+            {
+                _health = value;
+            }
+            else
+            {
+                _health = 0;
+                // TODO: Необходимо вызвать функцию смерти
+                if(tag != "Player")
+                {
+                    Destroy(_GOInstance);
+                }
+            }
+        }
+    }
+
+    public void SetDamage(float damage)
+    {
+        Health -= damage;
+    }
+}
